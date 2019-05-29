69f849086e093d7506877f329893973d9cbfccab
Aparna Singh <aparna.singh@vanderbilt.edu>
2019-05-29 13:28:03 -0500

    data science course assignment 1

---
+++ data_science_assignment1.r
@@ -0,0 +1,19 @@
+# assignment 1 Havard data science class
+#1. calculate the sum of first 100 integers
+
+n <- 100
+sum_100 = n*(n+1)/2
+sum_100
+##
+
+#2. calculate the sum of first 1000 integers
+
+n <- 1000
+sum_100 = n*(n+1)/2
+sum_100
+
+##
+n <- 1000
+x<-seq(1,n)
+
+
